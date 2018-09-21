40e590734fd289385af9e23775a9db699f3a8716
Austin Gill <notgnoshi@gmail.com>
2018-09-20 18:35:07 -0600

    Add hacky-as-shit script to convert notebook to PDF
    TODO:
    * Make less hacky
    * Error handling
    * Maybe use Python?
    * Clean up after self
    * Usage et al

---
+++ homework/convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Converts a jupyter notebook to a PDF
+
+# Convert all SVGs in the given directory to PDFs
+find "$1" -name '*.svg' -type f -exec bash -c 'convert "$0" "${0%.svg}.pdf"' {} \;
+
+# Convert notebook to LaTeX and replace \includegraphics{*.svg} with \includegraphics{*.pdf}
+jupyter nbconvert "$1/$1.ipynb" --stdout --to latex | sed '/\\includegraphics/{s/\.svg/\.pdf/g}' > "$1/$1.tex"
+
+# Save LaTeX output, rather than piping into pdflatex because it's nice to be able to edit it.
+
+# Attempt to compile the PDF
+cd "$1"
+latexmk -pdf "$1.tex"
