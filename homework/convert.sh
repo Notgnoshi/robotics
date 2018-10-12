42fba225f11b87ab429da34f8685c9d6bc998f04
Austin Gill <notgnoshi@gmail.com>
2018-10-12 00:02:03 -0600

    Use better SVG to PDF conversion tool

--- homework/convert.sh
+++ homework/convert.sh
@@ -3,7 +3,7 @@
 # Converts a jupyter notebook to a PDF
 
 # Convert all SVGs in the given directory to PDFs
-find "$1" -name '*.svg' -type f -exec bash -c 'convert "$0" "${0%.svg}.pdf"' {} \;
+find "$1" -name '*.svg' -type f -exec bash -c 'rsvg-convert -f pdf -o "${0%.svg}.pdf" "$0"' {} \;
 
 # Convert notebook to LaTeX and replace \includegraphics{*.svg} with \includegraphics{*.pdf}
 jupyter nbconvert "$1/$1.ipynb" --stdout --to latex | sed '/\\includegraphics/{s/\.svg/\.pdf/g}' > "$1/$1.tex"
